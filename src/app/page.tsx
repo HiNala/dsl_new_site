ac9dc67361803c23ab592c67da6a7f9eccb325ea
NalaBook <nalamaui30@gmail.com>
2025-07-20 05:29:52 -0700

    style: Increase title text size for greater impact
    - Increase mobile text from text-4xl to text-6xl
    - Increase tablet text from text-6xl to text-8xl
    - Increase desktop text from text-7xl to text-9xl
    - Make Digital Studio Labs title more prominent and bold
    - Improve visual hierarchy and brand presence

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       
       {/* Title - Left Side */}
       <div className="absolute top-1/4 left-8 md:left-16 -translate-y-1/2 z-10">
-        <h1 className="text-black font-light text-4xl md:text-6xl lg:text-7xl leading-none tracking-tight">
+        <h1 className="text-black font-light text-6xl md:text-8xl lg:text-9xl leading-none tracking-tight">
           <div>Digital</div>
           <div>Studio</div>
           <div>Labs</div>
